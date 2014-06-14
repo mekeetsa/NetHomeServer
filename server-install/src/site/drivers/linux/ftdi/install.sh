93606a6b68cdc53e42b813117a5501962db8b7fe
stefangs <stefangs@nethome.nu>
2014-06-15 01:45:54 +0200

    corrected install and fixed line endings

--- server-install/src/site/drivers/linux/ftdi/install.sh
+++ server-install/src/site/drivers/linux/ftdi/install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-cp $(SCRIPTPATH)/98-nethome.rules /etc/udev/rules.d/
-cp $(SCRIPTPATH)/ftdi_tellstick.sh /etc/
+cp ${SCRIPTPATH}/98-nethome.rules /etc/udev/rules.d/
+cp ${SCRIPTPATH}/ftdi_tellstick.sh /etc/
 chmod +x /etc/ftdi_tellstick.sh
-cp $(SCRIPTPATH)/ftdi_fhz1000.sh /etc/
+cp ${SCRIPTPATH}/ftdi_fhz1000.sh /etc/
 chmod +x /etc/ftdi_fhz1000.sh
-cp $(SCRIPTPATH)/ftdi_fhz1300.sh /etc/
+cp ${SCRIPTPATH}/ftdi_fhz1300.sh /etc/
 chmod +x /etc/ftdi_fhz1300.sh
-udevadm control –-reload-
+udevadm control --reload-rules
