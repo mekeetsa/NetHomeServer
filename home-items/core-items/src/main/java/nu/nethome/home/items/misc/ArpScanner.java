7629a83cd85a102c7a003c36e2a95f8c75b062be
Stefan G Strömberg <stefangs@nethome.nu>
2014-12-30 17:13:25 +0100

    Completed ArpScanner Item

--- home-items/core-items/src/main/java/nu/nethome/home/items/misc/ArpScanner.java
+++ home-items/core-items/src/main/java/nu/nethome/home/items/misc/ArpScanner.java
@@ -36,35 +36,78 @@ import java.util.logging.Logger;
 @HomeItemType("Ports")
 public class ArpScanner extends HomeItemAdapter implements HomeItem {
 
+    public static final String ARP_SCAN_MESSAGE = "ArpScan_Message";
+    public static final int MAX_ERROR_DISPLAY_SIZE = 30;
     private final String m_Model = ("<?xml version = \"1.0\"?> \n"
             + "<HomeItem Class=\"ArpScanner\"  Category=\"Ports\" >"
-            + "  <Attribute Name=\"Temperature\" 	Type=\"String\" Get=\"getValue\" Default=\"true\"  Unit=\"°C\" />"
+            + "  <Attribute Name=\"MacCount\" 	Type=\"String\" Get=\"getMacCount\" Default=\"true\" />"
             + "  <Attribute Name=\"ArpScan\"	Type=\"String\" Get=\"getExecName\" 	Set=\"setExecName\" />"
-            + "  <Attribute Name=\"LogFile\" Type=\"String\" Get=\"getLogFile\" 	Set=\"setLogFile\" />"
-            + "  <Action Name=\"Scan\" 	Method=\"scan\" />"
+            + "  <Attribute Name=\"ScanInterval\"	Type=\"String\" Get=\"getScanInterval\"  Set=\"setScanInterval\" />"
+            + "  <Action Name=\"Scan\" 	Method=\"reportScanResult\" />"
             + "</HomeItem> ");
 
     private static Logger logger = Logger.getLogger(ArpScanner.class.getName());
-    protected String execName = "/usr/bin/arp-scan -r 3 -b 2 -q --interface=eth0 --localnet";
+    private Timer scanTimer;
 
-    // Public attributes
-    protected double m_Value = 0.0;
-    protected String m_SensorName = "";
+    private String execName = "/usr/bin/arp-scan -r 3 -b 2 -q --interface=eth0 --localnet";
+    private String macCount = "";
+    long scanInterval = 30000;
 
     public ArpScanner() {
     }
 
-    /* (non-Javadoc)
-     * @see ssg.home.HomeItem#getModel()
-     */
     public String getModel() {
         return m_Model;
     }
 
-    /**
-     * @return Returns the m_Value.
-     */
-    public List<String> scan() {
+    @Override
+    public void activate() {
+        startTimer();
+    }
+
+    private void startTimer() {
+        scanTimer = new Timer("ArpScanner", true);
+        scanTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                reportScanResult();
+            }
+        }, scanInterval, scanInterval);
+    }
+
+    @Override
+    public void stop() {
+        stopTimer();
+        super.stop();
+    }
+
+    private void stopTimer() {
+        if (scanTimer != null) {
+            scanTimer.cancel();
+            scanTimer = null;
+        }
+    }
+
+    public String reportScanResult() {
+        try {
+            List<String> macs = null;
+            macs = scan();
+            String macString = "";
+            String separator = "";
+            for (String mac : macs) {
+                macString += separator + mac;
+                separator = ",";
+            }
+            server.send(server.createEvent(ARP_SCAN_MESSAGE, macString));
+            macCount = "" + macs.size();
+        } catch (ExecutionFailure executionFailure) {
+            macCount = executionFailure.getMessage().substring(0, Math.min(MAX_ERROR_DISPLAY_SIZE, executionFailure.getMessage().length()));
+            macCount += executionFailure.getMessage().length() > MAX_ERROR_DISPLAY_SIZE ? "..." : "";
+        }
+        return null;
+    }
+
+    public List<String> scan() throws ExecutionFailure {
         try {
             Runtime r = Runtime.getRuntime();
             Process proc = r.exec(execName);
@@ -74,35 +117,32 @@ public class ArpScanner extends HomeItemAdapter implements HomeItem {
             response.join(1000);
             return response.responseLines;
 
-        } catch (IOException|InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
             logger.warning("Failed to execute arp-scan: " + e.getMessage());
+            throw new ExecutionFailure(e.getMessage());
         }
-        return Collections.emptyList();
     }
 
-    /**
-     * @return Returns the m_SensorName.
-     */
-    public String getSensorName() {
-        return m_SensorName;
+    public String getMacCount() {
+        return macCount;
     }
 
-    /**
-     * @param SensorName The m_SensorName to set.
-     */
-    public void setSensorName(String SensorName) {
-        m_SensorName = SensorName;
-    }
-
-    /**
-     * @param execName the m_ExecName to set
-     */
     public void setExecName(String execName) {
         this.execName = execName;
     }
 
     public String getExecName() {
         return execName;
+    }
+
+    public String getScanInterval() {
+        return "" + (scanInterval / 1000);
+    }
+
+    public void setScanInterval(String scanInterval) {
+        stopTimer();
+        this.scanInterval = Long.parseLong(scanInterval) * 1000;
+        startTimer();
     }
 
     static class ResponseParser extends Thread {
@@ -131,5 +171,4 @@ public class ArpScanner extends HomeItemAdapter implements HomeItem {
             }
         }
     }
-
 }
--- home-items/core-items/src/test/java/nu/nethome/home/items/misc/ArpScannerTest.java
+++ home-items/core-items/src/test/java/nu/nethome/home/items/misc/ArpScannerTest.java
@@ -1,13 +1,23 @@
 package nu.nethome.home.items.misc;
 
+import nu.nethome.home.impl.InternalEvent;
+import nu.nethome.home.impl.LocalHomeItemProxy;
+import nu.nethome.home.item.ExecutionFailure;
+import nu.nethome.home.items.net.Message;
+import nu.nethome.home.system.Event;
+import nu.nethome.home.system.HomeService;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 /**
  *
@@ -42,10 +52,79 @@ public class ArpScannerTest {
             "10 packets received by filter, 0 packets dropped by kernel\n" +
             "Ending arp-scan 1.8.1: 256 hosts scanned in 4.033 seconds (63.48 hosts/sec). 8 responded";
 
+    private ArpScanner scanner;
+    private HomeService server;
+    private InternalEvent sentEvent;
+    private LocalHomeItemProxy itemProxy;
 
     @Before
     public void setUp() throws Exception {
+        scanner = spy(new ArpScanner());
+        itemProxy = new LocalHomeItemProxy(scanner);
+        sentEvent = new InternalEvent("Foo");
+        server = mock(HomeService.class);
+        doReturn(sentEvent).when(server).createEvent(anyString(), anyString());
+    }
 
+    @After
+    public void tearDown() throws Exception {
+        scanner.stop();
+    }
+
+    @Test
+    public void countAttributeEmptyBeforeScan() throws Exception {
+        assertThat(itemProxy.getAttributeValue("MacCount"), is(""));
+    }
+
+    @Test
+    public void countAttributeCountsMacAddresses() throws Exception {
+        scanner.activate(server);
+        doReturn(Arrays.asList("e4:f4:c6:09:2c:26", "80:ea:96:1f:03:b5")).when(scanner).scan();
+
+        scanner.reportScanResult();
+
+        assertThat(itemProxy.getAttributeValue("MacCount"), is("2"));
+    }
+
+    @Test
+    public void scansOnSpecifiedInterval() throws Exception {
+        doReturn(Arrays.asList("e4:f4:c6:09:2c:26", "80:ea:96:1f:03:b5")).when(scanner).scan();
+        scanner.scanInterval = 50L;
+        scanner.activate(server);
+
+        Thread.sleep(200L);
+
+        verify(server, atLeast(2)).send(sentEvent);
+        verify(server, atMost(4)).send(sentEvent);
+    }
+
+    @Test
+    public void reportsError() throws Exception {
+        scanner.activate(server);
+        doThrow(new ExecutionFailure("Test")).when(scanner).scan();
+
+        scanner.reportScanResult();
+
+        verify(server, times(0)).send(sentEvent);
+        assertThat(itemProxy.getAttributeValue("MacCount"), is("Test"));
+    }
+
+    @Test
+    public void sendsEvent() throws Exception {
+        scanner.activate(server);
+        doReturn(Arrays.asList("e4:f4:c6:09:2c:26", "80:ea:96:1f:03:b5")).when(scanner).scan();
+
+        scanner.reportScanResult();
+
+        verify(server).createEvent(ArpScanner.ARP_SCAN_MESSAGE, "e4:f4:c6:09:2c:26,80:ea:96:1f:03:b5");
+        verify(server).send(sentEvent);
+    }
+
+    @Test
+    public void canSetInterval() throws Exception {
+        itemProxy.setAttributeValue("ScanInterval", "60");
+        assertThat(itemProxy.getAttributeValue("ScanInterval"), is("60"));
+        assertThat(scanner.scanInterval, is(60000L));
     }
 
     @Test
