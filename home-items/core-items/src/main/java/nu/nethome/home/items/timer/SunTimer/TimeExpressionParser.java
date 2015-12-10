d9f4116ad791ba3cd411eab6599c8815fd732bdf
stefangs <stefangs@nethome.nu>
2015-12-10 21:59:16 +0100

    Can add and subtract times. Can select first or last time. Reports errors.

--- home-items/core-items/src/main/java/nu/nethome/home/items/timer/SunTimer/TimeExpressionParser.java
+++ home-items/core-items/src/main/java/nu/nethome/home/items/timer/SunTimer/TimeExpressionParser.java
@@ -10,16 +10,20 @@ import java.util.Map;
  */
 public class TimeExpressionParser {
 
+    private static final String TIME_EXPRESSION_SEPARATOR = ",";
     public static final String TIME_PERIOD_SEPARATOR = "->";
     public static final String TIME_SEPARATOR = ":";
+    private static final String SUM_SEPARATOR_INCLUDING_SEPARATOR = "(?=\\+|\\-)";
+    private static final String FIRST_TIME_ALTERNATIVE_SEPARATOR = "/";
+    private static final String LAST_TIME_ALTERNATIVE_SEPARATOR = "\\\\";
 
-    public List<SwitchTime> parseExpression(String expression) {
+    public List<SwitchTime> parseExpression(String expression) throws TimeExpressionException {
         return parseExpression(expression, Collections.<String, String>emptyMap());
     }
 
-    public List<SwitchTime> parseExpression(String expression, Map<String, String> variables) {
+    public List<SwitchTime> parseExpression(String expression, Map<String, String> variables) throws TimeExpressionException {
         ArrayList<SwitchTime> switchTimes = new ArrayList<>();
-        String[] switchTimeExpressions = replaceVariables(expression, variables).split(",");
+        String[] switchTimeExpressions = replaceVariables(expression, variables).split(TIME_EXPRESSION_SEPARATOR);
         for (String switchTimeExpression : switchTimeExpressions) {
             switchTimes.addAll(SwitchTime.parseSwitchTimes(switchTimeExpression));
         }
@@ -31,14 +35,26 @@ public class TimeExpressionParser {
         for (String variable : variables.keySet()) {
             result = result.replace(variable, variables.get(variable));
         }
+        result = result.replace("--", "+");
+        result = result.replace("+-", "-");
+        result = result.replace("-+", "-");
         return result;
+    }
+
+    public static class TimeExpressionException extends Exception {
+        public final String badExpression;
+
+        public TimeExpressionException(String badExpression) {
+            super("Cannot parse time expression: " + badExpression);
+            this.badExpression = badExpression;
+        }
     }
 
     public static class SwitchTime {
         private boolean on;
         private int value = 0;
 
-        public static List<SwitchTime> parseSwitchTimes(String expression) {
+        public static List<SwitchTime> parseSwitchTimes(String expression) throws TimeExpressionException {
             ArrayList<SwitchTime> switchTimes = new ArrayList<>();
             String[] switchTimeExpressions = expression.split(TIME_PERIOD_SEPARATOR);
             if (switchTimeExpressions.length > 0 && !switchTimeExpressions[0].isEmpty()) {
@@ -47,16 +63,60 @@ public class TimeExpressionParser {
             if (switchTimeExpressions.length > 1 && !switchTimeExpressions[1].isEmpty()) {
                 switchTimes.add(new SwitchTime(switchTimeExpressions[1], false));
             }
+            if (switchTimes.size() == 2 && switchTimes.get(0).value() >= switchTimes.get(1).value()) {
+                return Collections.emptyList();
+            }
             return switchTimes;
         }
 
-        public SwitchTime(String switchTimeExpression, boolean isOn) {
+        public SwitchTime(String switchTimeExpression, boolean isOn) throws TimeExpressionException {
             this.on = isOn;
-            String[] strings = switchTimeExpression.split(TIME_SEPARATOR);
-            if (strings.length > 1) {
-                value = Integer.parseInt(strings[1]) * 60 + Integer.parseInt(strings[0]) * 60 * 60;
-            } else if (strings.length == 1) {
-                value = Integer.parseInt(strings[0]) * 60;
+            this.value = parseLastTimeAlternative(switchTimeExpression);
+        }
+
+        private int parseLastTimeAlternative(String switchTimeExpression) throws TimeExpressionException {
+            int result = 0;
+            boolean isFirst = true;
+            String[] alternatives = switchTimeExpression.split(LAST_TIME_ALTERNATIVE_SEPARATOR);
+            for (String alternative : alternatives) {
+                result = isFirst ? parseFirstTimeAlternative(alternative) : Math.max(result, parseFirstTimeAlternative(alternative));
+                isFirst = false;
+            }
+            return result;
+        }
+
+        private int parseFirstTimeAlternative(String switchTimeExpression) throws TimeExpressionException {
+            int result = 0;
+            boolean isFirst = true;
+            String[] alternatives = switchTimeExpression.split(FIRST_TIME_ALTERNATIVE_SEPARATOR);
+            for (String alternative : alternatives) {
+                result = isFirst ? parseTimeSum(alternative) : Math.min(result, parseTimeSum(alternative));
+                isFirst = false;
+            }
+            return result;
+        }
+
+        private int parseTimeSum(String switchTimeExpression) throws TimeExpressionException {
+            int value = 0;
+            for (String singleTime : switchTimeExpression.split(SUM_SEPARATOR_INCLUDING_SEPARATOR)) {
+                value += parseTime(singleTime);
+            }
+            return value;
+        }
+
+        private int parseTime(String switchTimeExpression) throws TimeExpressionException {
+            try {
+                int value = 0;
+                String[] strings = switchTimeExpression.split(TIME_SEPARATOR);
+                int sign = switchTimeExpression.charAt(0) == '-' ? -1 : 1;
+                if (strings.length > 1) {
+                    value = Integer.parseInt(strings[1]) * 60 * sign + Integer.parseInt(strings[0]) * 60 * 60;
+                } else if (strings.length == 1) {
+                    value = Integer.parseInt(strings[0]) * 60;
+                }
+                return value;
+            } catch (NumberFormatException e) {
+                throw new TimeExpressionException(switchTimeExpression);
             }
         }
 
--- home-items/core-items/src/test/java/nu/nethome/home/items/timer/SunTimer/TimeExpressionParserTest.java
+++ home-items/core-items/src/test/java/nu/nethome/home/items/timer/SunTimer/TimeExpressionParserTest.java
@@ -1,7 +1,6 @@
 package nu.nethome.home.items.timer.SunTimer;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -9,10 +8,8 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.fail;
 
-/**
- *
- */
 public class TimeExpressionParserTest {
 
     private static final int HOUR = 60 * 60;
@@ -50,7 +47,7 @@ public class TimeExpressionParserTest {
         List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("01:02->03:04");
         assertThat(switchTimes.size(), is(2));
         assertThat(switchTimes.get(0).isOn(), is(true));
-        assertThat(switchTimes.get(0).value(), is(HOUR * 1 + MINUTE * 2));
+        assertThat(switchTimes.get(0).value(), is(HOUR + MINUTE * 2));
         assertThat(switchTimes.get(1).isOn(), is(false));
         assertThat(switchTimes.get(1).value(), is(HOUR * 3 + MINUTE * 4));
     }
@@ -60,7 +57,7 @@ public class TimeExpressionParserTest {
         List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("01:02->03:04,05:06->,->07:08");
         assertThat(switchTimes.size(), is(4));
         assertThat(switchTimes.get(0).isOn(), is(true));
-        assertThat(switchTimes.get(0).value(), is(HOUR * 1 + MINUTE * 2));
+        assertThat(switchTimes.get(0).value(), is(HOUR + MINUTE * 2));
         assertThat(switchTimes.get(1).isOn(), is(false));
         assertThat(switchTimes.get(1).value(), is(HOUR * 3 + MINUTE * 4));
         assertThat(switchTimes.get(2).isOn(), is(true));
@@ -74,24 +71,119 @@ public class TimeExpressionParserTest {
         HashMap<String, String> variables = new HashMap<>();
         variables.put("A", "01:02");
         variables.put("B", "03:04");
-        List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("A->B,05:00->A", variables);
+        List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("A->B,01:00->A", variables);
         assertThat(switchTimes.size(), is(4));
         assertThat(switchTimes.get(0).isOn(), is(true));
-        assertThat(switchTimes.get(0).value(), is(HOUR * 1 + MINUTE * 2));
+        assertThat(switchTimes.get(0).value(), is(HOUR + MINUTE * 2));
         assertThat(switchTimes.get(1).isOn(), is(false));
         assertThat(switchTimes.get(1).value(), is(HOUR * 3 + MINUTE * 4));
         assertThat(switchTimes.get(2).isOn(), is(true));
-        assertThat(switchTimes.get(2).value(), is(HOUR * 5 + MINUTE * 0));
+        assertThat(switchTimes.get(2).value(), is(HOUR));
         assertThat(switchTimes.get(3).isOn(), is(false));
-        assertThat(switchTimes.get(3).value(), is(HOUR * 1 + MINUTE * 2));
+        assertThat(switchTimes.get(3).value(), is(HOUR + MINUTE * 2));
     }
 
-    @Ignore
+    @Test
+    public void plusTime() throws Exception {
+        List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("->+2:07");
+        assertThat(switchTimes.size(), is(1));
+        assertThat(switchTimes.get(0).isOn(), is(false));
+        assertThat(switchTimes.get(0).value(), is(HOUR * 2 + MINUTE * 7));
+    }
+
+    @Test
+    public void minusTime() throws Exception {
+        List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("-2:07->");
+        assertThat(switchTimes.size(), is(1));
+        assertThat(switchTimes.get(0).isOn(), is(true));
+        assertThat(switchTimes.get(0).value(), is(HOUR * -2 + MINUTE * -7));
+    }
+
+    @Test
+    public void minusMinutes() throws Exception {
+        List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("-00:07->");
+        assertThat(switchTimes.size(), is(1));
+        assertThat(switchTimes.get(0).isOn(), is(true));
+        assertThat(switchTimes.get(0).value(), is(MINUTE * -7));
+    }
+
     @Test
     public void addTimes() throws Exception {
         List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("05:01+2:07+01:1");
         assertThat(switchTimes.size(), is(1));
         assertThat(switchTimes.get(0).isOn(), is(true));
         assertThat(switchTimes.get(0).value(), is(HOUR * 8 + MINUTE * 9));
+    }
+
+    @Test
+    public void subtractTimes() throws Exception {
+        List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("->05:01-2:07+01:1");
+        assertThat(switchTimes.size(), is(1));
+        assertThat(switchTimes.get(0).isOn(), is(false));
+        assertThat(switchTimes.get(0).value(), is(HOUR * 4 - MINUTE * 5));
+    }
+
+    @Test
+    public void negativeVariables() throws Exception {
+        HashMap<String, String> variables = new HashMap<>();
+        variables.put("A", "-01:02");
+        List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("10:00+A->10:00-A", variables);
+        assertThat(switchTimes.size(), is(2));
+        assertThat(switchTimes.get(0).isOn(), is(true));
+        assertThat(switchTimes.get(0).value(), is(HOUR * 8 + MINUTE * 58));
+        assertThat(switchTimes.get(1).isOn(), is(false));
+        assertThat(switchTimes.get(1).value(), is(HOUR * 11 + MINUTE * 2));
+    }
+
+    @Test
+    public void selectFirstTime() throws Exception {
+        List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("05:01/04:02/04:30");
+        assertThat(switchTimes.size(), is(1));
+        assertThat(switchTimes.get(0).isOn(), is(true));
+        assertThat(switchTimes.get(0).value(), is(HOUR * 4 + MINUTE * 2));
+    }
+
+    @Test
+    public void selectLastTime() throws Exception {
+        List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("01:01\\05:01\\04:02\\04:30");
+        assertThat(switchTimes.size(), is(1));
+        assertThat(switchTimes.get(0).isOn(), is(true));
+        assertThat(switchTimes.get(0).value(), is(HOUR * 5 + MINUTE));
+    }
+
+    @Test
+    public void mixCombinations() throws Exception {
+        HashMap<String, String> variables = new HashMap<>();
+        variables.put("A", "10:00");
+        variables.put("B", "15:00");
+        variables.put("C", "00:30");
+        List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("A-C/A/A+C->A+B+C-10:30+60\\15:59", variables);
+        assertThat(switchTimes.size(), is(2));
+        assertThat(switchTimes.get(0).isOn(), is(true));
+        assertThat(switchTimes.get(0).value(), is(HOUR * 9 + MINUTE * 30));
+        assertThat(switchTimes.get(1).isOn(), is(false));
+        assertThat(switchTimes.get(1).value(), is(HOUR * 16));
+    }
+
+    @Test
+    public void endBeforeStartDisappears() throws Exception {
+        List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("15:00->14:59");
+        assertThat(switchTimes.size(), is(0));
+    }
+
+    @Test
+    public void endSameAsStartDisappears() throws Exception {
+        List<TimeExpressionParser.SwitchTime> switchTimes = timeExpressionParser.parseExpression("15:00->15:00");
+        assertThat(switchTimes.size(), is(0));
+    }
+
+    @Test
+    public void reportsBadTime() {
+        try {
+            timeExpressionParser.parseExpression("15:00->14:59,19;00->20:00");
+            fail();
+        } catch (TimeExpressionParser.TimeExpressionException e) {
+            assertThat(e.badExpression, is("19;00"));
+        }
     }
 }
